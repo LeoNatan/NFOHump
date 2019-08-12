6ad348b2cfcaacb84629c5df43912afee4bf799b
Leo Natan <leo.natan@outlook.com>
2019-08-13 01:21:02 +0300

    Update script to support profile editor as well.

--- NFOHumpEmoji/Tampermonkey/NFOHumpEmoji.js
+++ NFOHumpEmoji/Tampermonkey/NFOHumpEmoji.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NFOHump Emoji
 // @namespace    http://nfohump.com/
-// @version      0.1
+// @version      0.1.1
 // @description  Adds support for Emojis in NFOHump forums.
 // @author       Leo Natan
 // @match        *://nfohump.com/forum/*
@@ -9,18 +9,34 @@
 // @grant        none
 // @downloadURL  https://raw.githubusercontent.com/LeoNatan/NFOHump/master/NFOHumpEmoji/Tampermonkey/NFOHumpEmoji.js
 // @updateURL    https://raw.githubusercontent.com/LeoNatan/NFOHump/master/NFOHumpEmoji/Tampermonkey/NFOHumpEmoji.js
+// @position     1
+// @noframes
 // @require      https://twemoji.maxcdn.com/v/latest/twemoji.min.js
 // ==/UserScript==
 
-let element = document.getElementById('qrform');
+var element = null;
 var textArea = null;
-if(element == null)
+
+function findQuickReply()
 {
+    if(element != null) return;
+
+    element = document.getElementById('qrform');
+    if(element != null)
+    {
+        textArea = document.getElementById('msg');
+    }
+}
+
+function findFullReply()
+{
+    if(element != null) return;
+
     const byName = document.getElementsByName('post');
 
     if(byName.length === 0)
     {
-        return false;
+        return;
     }
 
     element = byName[0];
@@ -29,17 +45,52 @@ if(element == null)
 
     if(byClass.length < 2)
     {
-        return false;
+        return;
     }
 
     textArea = byClass[1];
 }
-else
+
+function findProfileEditor()
 {
-    textArea = document.getElementById('msg');
+    if(element != null) return;
+
+    let byClass = document.getElementsByClassName('bodyline');
+
+    if(byClass.length === 0)
+    {
+        return;
+    }
+
+    if(byClass[0].children.length === 0 || byClass[0].children[0].nodeName !== 'FORM')
+    {
+        return;
+    }
+
+    element = byClass[0].children[0];
+
+    if(element.children.length === 0)
+    {
+        return;
+    }
+
+    byClass = element.getElementsByClassName('post');
+
+    for(let e of byClass)
+    {
+        if(e.nodeName === 'TEXTAREA')
+        {
+            textArea = e;
+            break;
+        }
+    }
 }
 
-if(element == null || element === undefined || textArea == null || textArea === undefined)
+findQuickReply();
+findFullReply();
+findProfileEditor();
+
+if(element == null || element === undefined || textArea == null || textArea === undefined || textArea.nodeName !== 'TEXTAREA' || element.nodeName != 'FORM')
 {
     return false;
 }
@@ -50,9 +101,14 @@ const oldOnSubmit = element.onsubmit;
 function parseEmojiCharacters()
 {
     const regex = /<img class="emoji" .*?src=\"(.*?)\"\/>/g;
-    textArea.value = twemoji.parse(textArea.value).replace(regex, " [img width=15]$1[/img] ");
+    textArea.value = twemoji.parse(textArea.value).replace(regex, " [img width=14]$1[/img] ");
 
-    return oldOnSubmit();
+    if(oldOnSubmit != null)
+    {
+        return oldOnSubmit();
+    }
+
+    return true;
 }
 
 element.onsubmit = parseEmojiCharacters;
