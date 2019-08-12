87aa08be327646b1e9cfedf1a08ae9a55e94571f
Leo Natan <leo.natan@outlook.com>
2019-08-12 12:36:16 +0300

    Add scripts

---
+++ NFOHumpEmoji/Tampermonkey/NFOHumpEmoji.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         NFOHump Emoji
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Adds support for Emojis in NFOHump forums.
+// @author       Leo Natan
+// @match        *://nfohump.com/forum/*
+// @match        *://www.nfohump.com/forum/*
+// @grant        none
+// @require      https://twemoji.maxcdn.com/v/latest/twemoji.min.js
+// ==/UserScript==
+
+let element = document.getElementById('qrform');
+var textArea = null;
+if(element == null)
+{
+    const byName = document.getElementsByName('post');
+
+    if(byName.length === 0)
+    {
+        return false;
+    }
+
+    element = byName[0];
+
+    const byClass = element.getElementsByClassName('post');
+
+    if(byClass.length < 2)
+    {
+        return false;
+    }
+
+    textArea = byClass[1];
+}
+else
+{
+    textArea = document.getElementById('msg');
+}
+
+if(element == null || element === undefined || textArea == null || textArea === undefined)
+{
+    return false;
+}
+
+const oldOnSubmit = element.onsubmit;
+//textArea.value = '';
+
+function parseEmojiCharacters()
+{
+    const regex = /<img class="emoji" .*?src=\"(.*?)\"\/>/g;
+    textArea.value = twemoji.parse(textArea.value).replace(regex, " [img width=15]$1[/img] ");
+
+    return oldOnSubmit();
+}
+
+element.onsubmit = parseEmojiCharacters;
