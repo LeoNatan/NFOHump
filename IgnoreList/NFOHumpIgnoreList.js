7fc2d3ef35192d7afefed6364fa1131ebf069ed0
Leo Natan <leo.natan@outlook.com>
2019-08-19 07:10:09 +0300

    Do not refresh pages when setting ignored users

--- IgnoreList/NFOHumpIgnoreList.js
+++ IgnoreList/NFOHumpIgnoreList.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NFOHump Ignore
 // @namespace    http://nfohump.com/
-// @version      1.0.1
+// @version      1.1
 // @description  Adds proper ignore list in NFOHump forums, where posts actually disappear.
 // @author       Leo Natan
 // @match        *://nfohump.com/forum/*
@@ -13,6 +13,8 @@
 // @noframes
 // @run-at document-body
 // ==/UserScript==
+
+const className = "hiddenByNFOHumpIgnore";
 
 if(localStorage.blocklist === null)
 {
@@ -34,26 +36,46 @@ const anchor = $('<a class="mainmenu" style="cursor: pointer;">Ignore list</a>')
     if (q != localStorage.blocklist)
     {
         localStorage.blocklist = q;
-        window.location.reload();
+        resetAndHideElements();
     }
 });
 
 const checkbox = $('<input style="margin: 0px; margin-left: 8px; margin-top: 1px;" type="checkbox" ' + (localStorage.isBlocklistEnabled == "true" ? 'checked' : '') + ' />').click(function () {
     localStorage.isBlocklistEnabled = (localStorage.isBlocklistEnabled == "true" ? "false" : "true");
-    window.location.reload();
+    resetAndHideElements();
 });
 
 $('#leftdiv > div.menuLeftContainer:first > ul').append($('<li style="vertical-align: middle;"></li>').append(anchor).append(checkbox));
 
-if (localStorage.isBlocklistEnabled == "true" && localStorage['blocklist'].length > 0)
-{
+const ignoreUser = $('<span>&nbsp;</span><img style="cursor: pointer; max-width: 10.5px;" src="https://twemoji.maxcdn.com/v/12.1.2/svg/1f6ab.svg" />').click(function(e) {
+    const clickedUserName = $(e.target).parent().children().first().html();
+
+    var arr = $.grep($.map(localStorage.blocklist.split(','), function(v) {
+        return $.trim(v);
+    }), function(e) { return e !== clickedUserName; });
+
+    arr.push(clickedUserName);
+
+    localStorage.blocklist = arr.join(', ');
+
+    resetAndHideElements();
+});
+
+//const userParents = $("a[title|='click to insert @mention']").parent();
+//userParents.append(ignoreUser);
+//userParents.css('display', 'flex').css('vertical-align', 'baseline');
+
+function hideElements() {
     $.each(localStorage.blocklist.split(','), function(k,v) {
         v = $.trim(v);
         if(window.location.href.includes('/viewtopic.php')) {
             $('span.nav > b > a:contains("'+v+'")').each(function() {
                 const x = $(this).parents('td:eq(0)').parent();
+                x.addClass(className);
                 x.hide();
+                x.next().addClass(className);
                 x.next().hide();
+                x.next().next().addClass(className);
                 x.next().next().hide();
             })
         }
@@ -62,3 +84,16 @@ if (localStorage.isBlocklistEnabled == "true" && localStorage['blocklist'].lengt
         }
     });
 }
+
+function resetHiddenElements() {
+    $('.' + className).show();
+}
+
+function resetAndHideElements() {
+    resetHiddenElements();
+    if (localStorage.isBlocklistEnabled == "true" && localStorage['blocklist'].length > 0) {
+        hideElements();
+    }
+}
+
+resetAndHideElements();
