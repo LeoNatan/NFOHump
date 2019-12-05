b0d585d3a5e2fd74cdafe929491e303f86f6a354
Leo Natan <leo.natan@outlook.com>
2019-12-05 22:01:50 +0200

    Add NFOCookies

---
+++ NFOCookies/NFOCookies.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         NFOHump Cookie Fixer
+// @namespace    http://nfohump.com/
+// @version      1.0
+// @description  Fixes NFOHump's bad cookie management.
+// @author       Leo Natan
+// @match        *://nfohump.com/forum/*
+// @match        *://www.nfohump.com/forum/*
+// @grant        none
+// @position     1
+// @noframes
+// @downloadURL  https://raw.githubusercontent.com/LeoNatan/NFOHump/master/NFOCookies/NFOCookies.js
+// @updateURL    https://raw.githubusercontent.com/LeoNatan/NFOHump/master/NFOCookies/NFOCookies.js
+// @require      https://cdn.jsdelivr.net/npm/js-cookie@beta/dist/js.cookie.min.js
+// ==/UserScript==
+
+const maxAllowed = 95;
+
+let forum_t = Cookies.get('forum_t');
+let outer = /a:(\d+):\{(.*)\}/g;
+let matches = outer.exec(forum_t);
+let pairCount = matches[1];
+
+if(pairCount <= maxAllowed)
+{
+    return;
+}
+
+let pairsStr = matches[2];
+
+let splitter = /(i:(\d*);i:(\d*);)/g;
+matches = pairsStr.matchAll(splitter);
+
+let pairs = [];
+for(const match of matches)
+{
+    pairs.push({post: match[2], timestamp: match[3]});
+}
+
+pairs.sort((a, b) => { return a.timestamp > b.timestamp ? 1 : -1 });
+pairs = pairs.slice(-maxAllowed);
+
+const serialized = pairs.map(a => "i:" + a.post + ";i:" + a.timestamp).join(';');
+const forCookie = "a:" + pairs.length + ":{" + serialized + "}";
+
+Cookies.set('forum_t', escape(forCookie), {path: '/forum', domain: '.nfohump.com'});
+Cookies.set('forum_f_all', pairs[0].timestamp, {path: '/forum', domain: '.nfohump.com'});
