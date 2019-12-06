25271f3175879de12415fdf2671d7f6b5a089605
Leo Natan <leo.natan@outlook.com>
2019-12-06 23:54:38 +0200

    Reset read forums on mark all as read

--- NFOCookies/NFOCookies.js
+++ NFOCookies/NFOCookies.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NFOHump Cookie Fixer
 // @namespace    http://nfohump.com/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Fixes NFOHump's bad cookie management.
 // @author       Leo Natan
 // @match        *://nfohump.com/forum/*
@@ -14,37 +14,56 @@
 // @require      https://cdn.jsdelivr.net/npm/js-cookie@beta/dist/js.cookie.min.js
 // ==/UserScript==
 
-const maxAllowed = 95;
+const _maxAllowed = 95;
 
 const forum_t = Cookies.get('forum_t');
 const outer = /a:(\d+):\{(.*)\}/g;
 let matches = outer.exec(forum_t);
-const pairCount = matches[1];
+let pairCount = 0;
+if(matches != null)
+{
+     pairCount = matches[1];
+}
 
 $('#leftdiv > div.menuLeftContainer:first > ul > li:first > a').text("" + pairCount);
-$('a[href="index.php?mark=forums"]').click(() => { return confirm("Do you want to mark all forums as read?"); });
+$('a[href="index.php?mark=forums"]').click(() => {
+    let reset = confirm("Do you want to mark all forums as read?");
+    if(reset)
+    {
+        limitReadCount(pairCount, 0, false);
+    }
+    return reset;
+});
 
-if(pairCount <= maxAllowed)
+function limitReadCount(currentCount, maxAllowed, resetAllCookie)
 {
-    return;
+    if(currentCount <= maxAllowed)
+    {
+        return;
+    }
+
+    const pairsStr = matches[2];
+
+    const splitter = /(i:(\d*);i:(\d*);)/g;
+    matches = pairsStr.matchAll(splitter);
+
+    let pairs = [];
+    for(const match of matches)
+    {
+        pairs.push({post: match[2], timestamp: match[3]});
+    }
+
+    pairs.sort((a, b) => { return a.timestamp > b.timestamp ? 1 : -1 });
+    pairs = pairs.slice(-maxAllowed);
+
+    const serialized = pairs.map(a => "i:" + a.post + ";i:" + a.timestamp).join(';');
+    const forCookie = "a:" + pairs.length + ":{" + serialized + "}";
+
+    Cookies.set('forum_t', escape(forCookie), {path: '/forum', domain: '.nfohump.com'});
+    if(resetAllCookie)
+    {
+        Cookies.set('forum_f_all', pairs[0].timestamp, {path: '/forum', domain: '.nfohump.com'});
+    }
 }
 
-const pairsStr = matches[2];
-
-const splitter = /(i:(\d*);i:(\d*);)/g;
-matches = pairsStr.matchAll(splitter);
-
-let pairs = [];
-for(const match of matches)
-{
-    pairs.push({post: match[2], timestamp: match[3]});
-}
-
-pairs.sort((a, b) => { return a.timestamp > b.timestamp ? 1 : -1 });
-pairs = pairs.slice(-maxAllowed);
-
-const serialized = pairs.map(a => "i:" + a.post + ";i:" + a.timestamp).join(';');
-const forCookie = "a:" + pairs.length + ":{" + serialized + "}";
-
-Cookies.set('forum_t', escape(forCookie), {path: '/forum', domain: '.nfohump.com'});
-Cookies.set('forum_f_all', pairs[0].timestamp, {path: '/forum', domain: '.nfohump.com'});
+limitReadCount(pairCount, _maxAllowed, true);
