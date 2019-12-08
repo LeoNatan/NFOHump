4db2b904ec1faea6053c0842a53b02b383ce4856
Leo Natan <leo.natan@outlook.com>
2019-12-08 13:22:02 +0200

    Fix escaping issue

--- NFOCookies/NFOCookies.js
+++ NFOCookies/NFOCookies.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NFOHump Cookie Fixer
 // @namespace    http://nfohump.com/
-// @version      1.0.3
+// @version      1.0.4
 // @description  Fixes NFOHump's bad cookie management.
 // @author       Leo Natan
 // @match        *://nfohump.com/forum/*
@@ -17,6 +17,7 @@
 const _maxAllowed = 95;
 
 const forum_t = Cookies.get('forum_t');
+let forum_f_all = Cookies.get('forum_f_all');
 const outer = /a:(\d+):\{(.*)\}/g;
 let matches = outer.exec(forum_t);
 let pairCount = 0;
@@ -25,7 +26,10 @@ if(matches != null)
      pairCount = matches[1];
 }
 
-$('#leftdiv > div.menuLeftContainer:first > ul > li:first > a').text("" + pairCount);
+const dateOptions = { dateStyle: 'short', timeStyle: 'short', hour12: false, };
+
+$('#leftdiv > div.menuLeftContainer:first > ul > li:first > a').html(pairCount + "<br />" + (new Date(forum_f_all * 1000)).toLocaleString(
+   'en-UK', dateOptions));
 $('a[href="index.php?mark=forums"]').click(() => {
     let reset = confirm("Do you want to mark all forums as read?");
     if(reset)
@@ -56,14 +60,22 @@ function limitReadCount(currentCount, maxAllowed, resetAllCookie)
     pairs.sort((a, b) => { return a.timestamp > b.timestamp ? 1 : -1 });
     pairs = pairs.slice(-maxAllowed);
 
-    const serialized = pairs.map(a => "i:" + a.post + ";i:" + a.timestamp).join(';');
+    const serialized = pairs.map(a => "i:" + a.post + ";i:" + a.timestamp).join(';') + ';';
     const forCookie = "a:" + pairs.length + ":{" + serialized + "}";
 
-    Cookies.set('forum_t', escape(forCookie), {path: '/forum', domain: '.nfohump.com'});
+    var cookieSetter = Cookies.withConverter({
+        write: function (value, name) {
+            return escape(value);
+        }
+    })
+    cookieSetter.set('forum_t', forCookie, {path: '/forum', domain: '.nfohump.com'});
     if(resetAllCookie)
     {
         Cookies.set('forum_f_all', pairs[0].timestamp, {path: '/forum', domain: '.nfohump.com'});
     }
+
+    $('#leftdiv > div.menuLeftContainer:first > ul > li:first > a').html(pairs.length + "<br />" + (new Date(pairs[0].timestamp * 1000)).toLocaleString(
+   'en-UK', dateOptions));
 }
 
 limitReadCount(pairCount, _maxAllowed, true);
